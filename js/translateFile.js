6eccc14f98fda7aafc495b0518ecade3b669c0e5
Khadejaa Al-Shehri <khadejaa.cs@gmail.com>
2020-04-22 13:37:46 +0300

    Add files via upload
    add translate file

---
+++ js/translateFile.js
@@ -0,0 +1,321 @@
+var arrLang = new Array();
+arrLang['en'] = new Array();
+arrLang['ar'] = new Array();
+
+// English navbar content
+arrLang['en']['home'] = 'Home';
+arrLang['en']['about'] = 'About Me';
+arrLang['en']['contact'] = 'Contact Me';
+arrLang['en']['resume'] = 'My Resume';
+arrLang['en']['edu'] = 'Education';
+arrLang['en']['exp'] = 'Volunteer Experiences';
+arrLang['en']['skills'] = 'Skills';
+arrLang['en']['awards'] = 'Awards';
+arrLang['en']['services'] = 'My Services';
+arrLang['en']['achieve'] = 'My Last Achievements';
+arrLang['en']['blog'] = 'My Blog';
+
+// Arabic navbar content
+arrLang['ar']['home'] = 'الرئيسية'
+arrLang['ar']['about'] = 'عني';
+arrLang['ar']['contact'] = 'التواصل';
+arrLang['ar']['resume'] = 'سيرتي'
+arrLang['ar']['edu'] = 'التعليم';
+arrLang['ar']['exp'] = 'خبراتي التطوعية';
+arrLang['ar']['skills'] = 'مهاراتي';
+arrLang['ar']['awards'] = 'جوائزي';
+arrLang['ar']['services'] = 'خدماتي';
+arrLang['ar']['achieve'] = 'آخر انجازاتي';
+arrLang['ar']['blog'] = 'مدونتي';
+
+//English home section
+arrLang['en']['greating'] = 'Hey! I am'
+arrLang['en']['myName'] = 'Khadejaa'
+arrLang['en']['myRole'] = 'Computer Science Student, Full Stack Developer, EV3 Robot trainer, Blogger And More'
+
+// //Arabic home section
+arrLang['ar']['greating'] = 'مرحبا ، أنا '
+arrLang['ar']['myName'] = 'خديجة'
+arrLang['ar']['myRole'] = 'طالبة علوم حاسب ، مطورة مواقع ، مدربة روبوتات ، مٌدونة وأكثر '
+
+
+//English about me section
+arrLang['en']['aboutMe'] = 'About Me'
+arrLang['en']['headOfAbout'] = 'Computer Science is my other family that i chose it.'
+arrLang['en']['bodyOfAbout'] = 'I always found my self inside the lines of code and completely focus during programming and forget the world. I love the process of learning new concepts, skills and bake a cake.lately, I start learning drawing and the image on the left is my favorate painting that reflect how i see the world (half orgnized half messy)'
+
+//Arabic about me section
+arrLang['ar']['aboutMe'] = 'من أنا ؟'
+arrLang['ar']['headOfAbout'] = 'علوم الحاسب الآلي هو عائلتي الثانية التي اخترتها'
+arrLang['ar']['bodyOfAbout'] = 'لطالما وجدت نفسي منغمسة بين سطور الأكواد البرمجية وقد نسيت العالم من حولي ، أحب التعلم وتدهشني عملية تعلم المفاهيم والعلوم الجديدة و المهارات وأهوى خبز الكعك مؤخراً بدأت بتعلم الرسم الإلكتروني ، الصورة المجاورة هي أيقونتي الحالية وهي تعكس كيف أرى العالم بنصفيه المنظم والفوضوي '
+
+
+//English resume section
+//Education section
+arrLang['en']['education'] = 'Education'
+
+arrLang['en']['BachelorDate'] = '2013-2017'
+arrLang['en']['Bachelor'] = "Bachelor's Degree in Computer Science"
+arrLang['en']['UQU'] = 'Umm Al-Qura University'
+
+arrLang['en']['MasterDate'] = '2019- 2022 (Expected)'
+arrLang['en']['Master'] = "Master's Degree in Computer Science"
+arrLang['en']['KAU'] = 'King Abdulaziz University'
+
+arrLang['en']['FEWDdate'] = '2018'
+arrLang['en']['FEWD'] = 'Front-End Web Developer Nanodegree'
+arrLang['en']['Udacity'] = 'Udacity'
+
+arrLang['en']['iOSdate'] = '2019'
+arrLang['en']['iOS'] = 'iOS Developer Nanodegree Program'
+arrLang['en']['Udacity'] = 'Udacity'
+
+
+//Arabic resume section
+//Education section
+arrLang['ar']['education'] = 'التعليم'
+
+arrLang['ar']['BachelorDate'] = '٢٠١٣-٢٠١٧'
+arrLang['ar']['Bachelor'] = 'بكالوريس علوم الحاسب الآلي'
+arrLang['ar']['UQU'] = 'جامعة أم القرى'
+
+arrLang['ar']['MasterDate'] = '٢٠١٩-٢٠٢٢ (متوقع)'
+arrLang['ar']['Master'] = 'ماجستير علوم الحاسب الآلي'
+arrLang['ar']['KAU'] = 'جامعة الملك عبدالعزيز'
+
+arrLang['ar']['FEWDdate'] = '٢٠١٨'
+arrLang['ar']['FEWD'] = 'درجة النانو في تطوير المواقع'
+arrLang['ar']['Udacity'] = 'يوداستي'
+
+arrLang['ar']['iOSdate'] = '٢٠١٩'
+arrLang['ar']['iOS'] = 'درجة النانو في تطوير تطبيقات آي او اس'
+arrLang['ar']['Udacity'] = 'يوداستي'
+
+
+//ِEnglish experience section
+arrLang['en']['Experiences']='My Volunteer Experiences'
+
+arrLang['en']['akamDate']='2017-2019'
+arrLang['en']['akam']='A Member In Akam Team'
+arrLang['en']['wa3i']='wa3i'
+
+arrLang['en']['SEUdate']='2015-2017'
+arrLang['en']['SEU']='SEU Leader'
+arrLang['en']['College']='College Of Computers And Information Systems'
+
+arrLang['en']['coffeeDate']='2016-2017'
+arrLang['en']['coffee']="a Member Then Leader of 'After The Coffee' Reading Club"
+arrLang['en']['Rushd']='Rushd Club In Um Al-Qraa University'
+
+
+//Arabic experience section
+arrLang['ar']['Experiences']='خبراتي التطوعية'
+
+arrLang['ar']['akamDate']='٢٠١٧-٢٠١٩'
+arrLang['ar']['akam']='عضوة في فريق آكام التطوعي'
+arrLang['ar']['wa3i']='جمعية واعي'
+
+arrLang['ar']['SEUdate']='٢٠١٥-٢٠١٧'
+arrLang['ar']['SEU']='رئيسة نادي روح اتحاد الخبرات'
+arrLang['ar']['College']='كلية الحاسب الآلي ونظم المعلومات'
+
+arrLang['ar']['coffeeDate']='٢٠١٦-٢٠١٧'
+arrLang['ar']['coffee']='عضوة ثم قائدة لمقهى القراءة التابع لنادي رشد'
+arrLang['ar']['Rushd']='نادي رشد بجامعة أم القرى'
+
+//English skills section
+arrLang['en']['skills'] = 'Skills'
+
+arrLang['en']['Programing'] = 'Programing'
+arrLang['en']['Discussion'] = 'Managing Dialogue and Discussion'
+arrLang['en']['speek'] = 'Public Speak'
+arrLang['en']['self'] = 'Self Learning'
+arrLang['en']['Teaching'] = 'Teaching and Training'
+arrLang['en']['sketch'] = 'Digital Sketch'
+
+
+
+//Arabic skills section
+arrLang['ar']['skills'] = 'مهارتي'
+
+arrLang['ar']['Programing'] = 'البرمجة'
+arrLang['ar']['Discussion'] = 'إدارة الحوار والنقاش'
+arrLang['ar']['speek'] = 'الإلقاء والتقديم'
+arrLang['ar']['self'] = 'التعلم الذاتي'
+arrLang['ar']['Teaching'] = 'التعليم والتدريب'
+arrLang['ar']['sketch'] = 'الرسم الالكتروني'
+
+
+//English awards section
+arrLang['en']['Awards'] = 'Awards'
+
+arrLang['en']['LeaderDate'] = '2016'
+arrLang['en']['leaders'] = 'Top 5 Best leaders'
+arrLang['en']['leadersProgram'] = 'Be Leader Program'
+
+arrLang['en']['medalDate'] = '2019'
+arrLang['en']['medal'] = 'The Second Medal In EOSS| Research Track'
+arrLang['en']['FCIT'] = 'Faculty of Computing and Information Technology | KAU'
+
+//Arabic awards section
+arrLang['ar']['Awards'] = 'جوائز حصلت عليها'
+
+arrLang['ar']['LeaderDate'] = '2016'
+arrLang['ar']['leaders'] = 'وحده من أفضل خمس قائدات'
+arrLang['ar']['leadersProgram'] = 'برنامج كوني قائدة'
+
+arrLang['ar']['medalDate'] = '2019'
+arrLang['ar']['medal'] = 'الميدالية الثانية في مسار الأبحاث العلمية'
+arrLang['ar']['FCIT'] = 'كلية الحاسبات ونظم المعلومات بجامعة الملك عبدالعزيز'
+
+
+//English Services section
+arrLang['en']['Services']='Services'
+
+arrLang['en']['Teaching']='Teaching'
+arrLang['en']['TeachingDescription']='If you have any question related to computer sciences courses you can ask me to help you understand it .'
+
+arrLang['en']['web']='Web Developer'
+arrLang['en']['webDescription']='Do you like my personal website? I can develop one for you.'
+
+arrLang['en']['EV3']='EV3 Robot Training'
+arrLang['en']['EV3Description']='Do you want to learn how to programing ev3 robot ? I can help you.'
+
+//Arabic services section
+arrLang['ar']['Services']='خدمات أستطيع تقدميها لك'
+
+arrLang['ar']['Teaching']='دروس خصوصية'
+arrLang['ar']['TeachingDescription']='هل تواجهين أي صعوبة أو لديك أي سؤال يتعلق بعلوم الحاسب؟ أستطيع مساعدتك لتتخطي ذلك'
+
+arrLang['ar']['web']='تطوير المواقع'
+arrLang['ar']['webDescription']='هل أعجبك تصميم موقعي الشخصي؟ أستطيع تطوير وتصميم موقع لك '
+
+arrLang['ar']['EV3']='برمجة الروبوتات'
+arrLang['ar']['EV3Description']='هل لديك اهتمام بتعلم أساسيات برمجة الربوتات؟ يسعدني مساعدتك في ذلك'
+
+//ِEnglish My Last Achievements section
+arrLang['en']['Achievements'] = 'My Last Achievements'
+arrLang['en']['quotes'] = 'Who you are tomorrow begins with what you do today. – Tim Fargo'
+
+arrLang['en']['iosCertificate'] = 'Certificate of iOS Nanodegree'
+arrLang['en']['self'] = 'Self Learnin'
+
+arrLang['en']['fog'] = 'Above The Fog'
+arrLang['en']['fogDescription'] = 'Digital Sketch, One of my favorite digital drawings.'
+
+arrLang['en']['cs'] = 'CS Student Projects'
+arrLang['en']['csDescription'] = 'Web Design, Web-based system for FCIT of King Abdulaziz University Students that collecting there projects in one place'
+
+arrLang['en']['ML'] = 'Certificate of Machine Learning Course'
+arrLang['en']['self'] = 'Self Learning'
+
+arrLang['en']['reading'] = 'Reading Workshop'
+arrLang['en']['readingDescription'] = 'Public Speak, One of my favorite workshop that i presented it more than once.'
+
+arrLang['en']['calander'] = 'Digital Calendar'
+arrLang['en']['calanderDescription'] = 'My first experience to designing arabic digital calendar that work friendly with ipad and its pen'
+
+// Arabic My Last Achievements section
+arrLang['ar']['Achievements'] = 'من آخر انجازاتي'
+arrLang['ar']['quotes'] = 'من أنت غدا يبدأ بما تفعله اليوم. - تيم فارجو'
+
+arrLang['ar']['iosCertificate'] = 'شهادة درجة النانو في برمجة التطبيقات'
+arrLang['ar']['self'] = 'تعلم ذاتي'
+
+arrLang['ar']['fog'] = 'فوق الضباب'
+arrLang['ar']['fogDescription'] = 'رسم الكتروني ، واحدة من رسماتي المفضلة'
+
+arrLang['ar']['cs'] = 'مشاريع طلبة علوم الحاسب الآلي'
+arrLang['ar']['csDescription'] = 'تصميم المواقع، موقع لجمع مشاريع طلاب وطالبات علوم الحاسب الآلي بجامعة الملك عبدالعزيز'
+
+arrLang['ar']['ML'] = 'شهادة انجاز مادة تعلم الآلة من جامعة ستانفورد'
+arrLang['ar']['self'] = 'تعلم ذاتي'
+
+arrLang['ar']['reading'] = 'ورشة عمل القراءة (خير جليس)'
+arrLang['ar']['readingDescription'] = 'وحده من أفضل ورش العمل التي قدمتها أكثر من مره عن القراءة'
+
+arrLang['ar']['calander'] = 'تقويم رقمي'
+arrLang['ar']['calanderDescription'] = 'تجربتي الأولى في تصميم تقويم رقمي يستخدم على أجهزة الآيباد مع القلم بشكل مرن وسلس'
+
+//English blog section
+arrLang['en']['blog']='My Blog'
+arrLang['en']['quotesBlog']='The writing is my way to express my ideas and save my memories'
+
+arrLang['en']['Graduation']='Our Graduation Story (undergraduate project)'
+arrLang['en']['wadi']='My experience in summer training in Wadi Makkah'
+arrLang['en']['30days']='30 Days without social media'
+
+//Arabic blog section
+
+arrLang['ar']['blog']='مدونتي'
+arrLang['ar']['quotesBlog']= 'الكتابة هي وسيلتي وطريقتي المفضلة للتعبير عن أفكاري وحفظ أهم ذكرياتي'
+
+arrLang['ar']['Graduation']='حكاية تخرجنا - مشروع البكاولوريس'
+arrLang['ar']['wadi']='تجربتي في التدريب الصيفي بوادي مكة'
+arrLang['ar']['30days']='٣٠ يوماً بدون شبكات اجتماعية'
+
+
+//English thay Said section
+arrLang['en']['Said']='They Said'
+
+arrLang['en']['fatimaSaid']='I enjoyed working with Khadejaa during our graduation project. She is a positive, wonderful, honest person. She focuses on understanding important things and always trying to achieve her tasks in the best way she can. work with her gave me both wonderfully joy and great experience.'
+arrLang['en']['Fatima']='Fatima Aljaber'
+
+arrLang['en']['saraSaid']='Khadejaa is highly intelligent with excellent analytical skills, who attends to the fine details of the problem to be solved.Honesty and integrity are the hallmark of her character'
+arrLang['en']['sara']='Sarah Al-Shareef, Ph.D.'
+arrLang['en']['asPart']='As a part from recommendation letter. To read full recommendation'
+arrLang['en']['clickhere']='click here'
+
+arrLang['en']['ranadahSaid']='Khadejaa is highly intelligent with excellent analytical skills, who attends to the fine details of the problem to be solved. Honesty and integrity are the hallmark of her character.'
+arrLang['en']['randah']='Randah Alharbi'
+
+//Arabic they said section
+arrLang['ar']['Said']='قالوا عني'
+
+arrLang['ar']['fatimaSaid']='استمتعت بالعمل مع خديجة خلال مشروع التخرج ، شخصية ايجابية ، رائعة وصادقة ،تركز على فهم الأشياء المهمة وتعمل على تحقيق مهامها بأفضل الأحوال ، عملنا معاً كان تجربة رائعة .'
+arrLang['ar']['Fatima']='فاطمة الجابر'
+
+arrLang['ar']['saraSaid']='خديجة ذكية للغاية ولديها مهارات تحليلية ممتازة ،  تهتم بالتفاصيل الدقيقة لحل لمشكلة . الصدق والنزاهة هما السمة المميزة لشخصيتها.'
+arrLang['ar']['sara']='د.سارة الشريف'
+arrLang['ar']['asPart']=''
+arrLang['ar']['clickhere']=''
+
+arrLang['ar']['ranadahSaid']= "خديجة من أكثر الأشخاص الذين أعرفهم تكيُفاً. لقد مرت برحلة طويله من الصعاب ورأيتها وهي تقفز بينها برشاقة وخفّة روح وإصرار عظيم. أفتخر برحلتها وكفاحا ونجاحها. عرفتهُا طالبة في نفس قسمي ثم صديقة وفيّه شبيهة لروحي وقريبة منها. أسأل الله أن يحفظها ويزيدها ويبلغها المُنى وماتحب وتأمل"
+arrLang['ar']['randah']='أ.رندة الأحمدي'
+
+
+//English contact section
+arrLang['en']['contactMe']='Contact Me'
+arrLang['en']['Address']='Address'
+arrLang['en']['makkah']='Makkah, KSA'
+arrLang['en']['number']='Contact Number'
+arrLang['en']['email']='Email Address'
+
+
+//Arabic contact section
+arrLang['ar']['contactMe']='للتواصل معي'
+arrLang['ar']['Address']='العنوان'
+arrLang['ar']['makkah']='المملكة العربية السعودية ، مكة المكرمة'
+arrLang['ar']['number']='رقم الجوال'
+arrLang['ar']['email']='البريد الالكتروني'
+
+//ِEnglish footer section
+arrLang['en']['maillist']="Monthly, you'll get some of inspiring and motivating ideas and some reviews about my favorite books."
+arrLang['en']['Subscribe']='Subscribe'
+arrLang['en']['Copyright']=''
+
+//Arabic footer section
+arrLang['ar']['maillist']='شهرياً ستصلك رسائل بأفكار ملهمة ومحفزة وبعض المراجعات لأهم الكتب التي قرأتها'
+arrLang['ar']['Subscribe']='اشتراك'
+arrLang['ar']['Copyright']=''
+
+// Process translation
+$(function() {
+      $('.translate').click(function() {
+        var lang = $(this).attr('id');
+
+        $('.lang').each(function(index, item) {
+          $(this).text(arrLang[lang][$(this).attr('key')]);
+        });
+      });
+    });
